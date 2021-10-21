1fb4b22d0f7cbff13c3f3722e69589ba2296803d
Armen Mesropyan <amesropyan@bangits.com>
2021-10-21 15:11:50 +0400

    Added routing

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -2,7 +2,7 @@ import createStore from '@/adapter/redux/store/store';
 import { DiContainer } from '@/di';
 import { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
-import { SignInContainer } from './auth';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   const [store, setStore] = useState(null);
@@ -26,7 +26,26 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <SignInContainer />
+      <Router basename='/game'>
+        <Switch>
+          <Route
+            path='/providers'
+            render={({ match: { url } }) => (
+              <>
+                <Route path={`${url}/`} exact>
+                  <div>Providers</div>
+                </Route>
+
+                <Route path={`${url}/add`} exact>
+                  <div>Provider add</div>
+                </Route>
+              </>
+            )}
+          />
+
+          <Redirect to='/providers' />
+        </Switch>
+      </Router>
     </Provider>
   );
 };
