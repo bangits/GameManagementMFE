bbb4acc114fc6034e61b67de02ed5e2ee41636c1
Ruben <rubenabelyan.bangits@gmail.com>
2021-11-18 12:59:33 +0400

    eslint config implementation

--- src/validators/addProviderValidations.ts
+++ src/validators/addProviderValidations.ts
@@ -1,12 +1,17 @@
 import { AddProviderViewModel } from '@/models';
+import { UseValidationTranslationReturnValue } from '@atom/common';
 import { array, number, object, SchemaOf, string } from 'yup';
 
-export const addProviderValidationSchema: SchemaOf<AddProviderViewModel> = object({
-  name: string().required('Required field').max(40, 'The maximum length is 40').min(7, 'The minimum symbols is 7'),
-  logo: string().required('Required field'),
-  providerCurrencies: array().of(number()).min(1, 'Required field').required('Required field'),
-  defaultCurrency: number().min(1, 'Required field').required('Required field'),
-  targetMarkets: array().of(number()),
-  certifiedCountries: array().of(number()),
-  restrictedCountries: array().of(number())
-});
+export const getLoginValidationSchema = async (
+  t: UseValidationTranslationReturnValue
+): Promise<SchemaOf<AddProviderViewModel>> => {
+  return object({
+    name: string().required(t.required()).max(40,t.max(40)).min(7, t.min(6)),
+    logo: string().required(t.required()),
+    providerCurrencies: array().of(number()).min(1, t.required()).required(t.required()),
+    defaultCurrency: number().min(1, t.required()).required(t.required()),
+    targetMarkets: array().of(number()),
+    certifiedCountries: array().of(number()),
+    restrictedCountries: array().of(number())
+  });
+};
