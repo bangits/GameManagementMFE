d77dcf28d357a2637c0d4b771f818edff1821757
Armen Mesropyan <amesropyan@bangits.com>
2021-12-15 20:04:41 +0400

    Added ProviderGamesContainer

--- src/view/pages/provider/details/ProviderDetails.tsx
+++ src/view/pages/provider/details/ProviderDetails.tsx
@@ -5,11 +5,11 @@ import {
   PageWrapper,
   ProviderDetails as ProviderDetailsPage,
   ProviderDetailsProps as ProviderDetailsPageProps,
-  ProviderGames,
   ProvidersGeneralInfo,
   ProvidersGeneralInfoProps
 } from '@atom/design-system';
 import React, { FC, useMemo } from 'react';
+import { ProviderGamesContainer } from './provider-games';
 
 export interface ProviderDetailsProps {
   data: ProviderDetailsViewModel;
@@ -168,129 +168,7 @@ const ProviderDetails: FC<ProviderDetailsProps> = ({
   return (
     <PageWrapper>
       <ProviderDetailsPage
-        gamesTabContent={
-          <ProviderGames
-            searchInputMaxLength={30}
-            translations={{
-              addGame: 'Add Game',
-              noGames: 'No Games',
-              search: 'Search'
-            }}
-            gameTypes={[
-              {
-                id: 1,
-                name: 'Main Category',
-                gameCount: 9
-              },
-              {
-                id: 2,
-                name: 'Category',
-                gameCount: 29
-              }
-            ]}
-            games={[
-              {
-                id: 25,
-                name: 'EGT',
-                icon: null
-              },
-              {
-                id: 26,
-                name: 'EGT',
-                icon: null
-              },
-              {
-                id: 27,
-                name: 'EGT',
-                icon: null
-              },
-              {
-                id: 28,
-                name: 'EGT',
-                icon: null
-              },
-              {
-                id: 29,
-                name: 'EGT',
-                icon: null
-              },
-              {
-                id: 30,
-                name: 'քոեռտըւ',
-                icon: null
-              },
-              {
-                id: 31,
-                name: 'ԱԱԱՍՍՍԲԲԲԲԲ',
-                icon: null
-              },
-              {
-                id: 32,
-                name: '                                                 1',
-                icon: null
-              },
-              {
-                id: 33,
-                name: 'սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ  ',
-                icon: null
-              },
-              {
-                id: 34,
-                name: 'սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ սլօտ %  ',
-                icon: null
-              },
-              {
-                id: 35,
-                name: '!@#$%^&&^%$#@#$F%^%$#',
-                icon: null
-              },
-              {
-                id: 36,
-                name: '!@#$%^&&^%$#@#$F%^%$#',
-                icon: null
-              },
-              {
-                id: 37,
-                name: '!@#$%^&&^%$#@#$F%^%$#',
-                icon: null
-              },
-              {
-                id: 38,
-                name: '!@#$%^&&^%$#@#$F%^%$#',
-                icon: null
-              },
-              {
-                id: 39,
-                name: '!@#$%^&&^%$#@#$F%^%$#',
-                icon: null
-              },
-              {
-                id: 40,
-                name: '!@#$%^&&^%$#@#$F%^%$#',
-                icon: null
-              },
-              {
-                id: 41,
-                name: 'stringfg',
-                icon: null
-              },
-              {
-                id: 42,
-                name: 'string',
-                icon: null
-              },
-              {
-                id: 43,
-                name: 'string',
-                icon: null
-              }
-            ]}
-            onChange={console.log}
-            onGameClick={console.log}
-            onAddGameClick={console.log}
-            isLoadingGames={false}
-          />
-        }
+        gamesTabContent={<ProviderGamesContainer />}
         noDataText={t.get('emptyValue')}
         totalGameCount={`${data.gameCount || t.get('emptyValue')}`}
         creationDate={data.creationDate}
