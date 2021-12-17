2d8a4c59bbf2fd355fc0c9af1cbee92a109cd150
Ruben <rubenabelyan.bangits@gmail.com>
2021-12-17 23:15:55 +0400

    fixed game add details

--- src/view/pages/game/add/AddGame.tsx
+++ src/view/pages/game/add/AddGame.tsx
@@ -65,6 +65,9 @@ const AddGame: FC<AddGameProps> = ({ onSubmit, validationSchema, providerId }) =
       }
     };
 
+    console.log(GameTypesSelect, ProviderSelect, GameVolatilitiesSelect, GameClassSelect);
+    
+
     return [
       ...(!providerId
         ? [
