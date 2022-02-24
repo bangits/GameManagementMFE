d26dfd4b53efab6b6caff0e6b0c6d59ed7afbcf0
Ruben <ru.abelyan@mail.ru>
2022-02-24 15:29:55 +0400

    changes gameTypes,gameNames,features and gameClass es selects

--- src/view/components/selects/GameClassSelect.tsx
+++ src/view/components/selects/GameClassSelect.tsx
@@ -1,14 +1,26 @@
 import { gameApi } from '@/adapter/redux/api';
+import { GameManagementContext } from '@/atom-game-management';
+import { GetClassNamesViewModel } from '@/view/models/view-models';
 import { CustomSelect, CustomSelectProps, useTranslation } from '@atom/common';
+import { useContext, useEffect, useMemo, useState } from 'react';
 
 export const GameClassSelect = (props: CustomSelectProps) => {
-  const t = useTranslation();
 
-  const { data: classNames } = gameApi.useGetClassNamesQuery({});
+  const t = useTranslation();
+  const { gameUseCase } = useContext(GameManagementContext);
+
+  const [gameClasses, setGameClasses] = useState<GetClassNamesViewModel>([]);
+
+  const selectOptions = useMemo(() => gameClasses.map((c) => ({ value: c.value, label: c.label })), [gameClasses]);
+
+  useEffect(() => {
+    gameUseCase.getClassNames().then(setGameClasses);
+  }, []);
+
 
   return (
     <>
-      <CustomSelect {...props} fullWidth options={classNames || []} inputLabel={t.get('class')} />
+      <CustomSelect {...props} fullWidth options={selectOptions || []} inputLabel={t.get('class')} />
     </>
   );
 };
