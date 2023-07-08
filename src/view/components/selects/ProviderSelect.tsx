39fe2d187d1434462f38e93d1bdcb4417a0c5cda
Armen Mesropyan <amesropyan@bangits.com>
2023-07-08 20:35:10 +0400

    feat: add valueProp for game and provider selects

--- src/view/components/selects/ProviderSelect.tsx
+++ src/view/components/selects/ProviderSelect.tsx
@@ -2,16 +2,21 @@ import { GameManagementContext, GetProviderNamesViewModel } from '@/atom-game-ma
 import { CustomSelect, CustomSelectProps, useTranslation } from '@atom/common';
 import { useContext, useEffect, useMemo, useState } from 'react';
 
-export const ProviderSelect = ({
-  isMain = true,
-  ...props
-}: Omit<CustomSelectProps & { isMain?: boolean }, 'options'>) => {
+export interface ProviderSelectProps extends Omit<CustomSelectProps, 'options'> {
+  isMain?: boolean;
+  valueProp?: 'label' | 'value';
+}
+
+export const ProviderSelect = ({ isMain = true, valueProp = 'value', ...props }: ProviderSelectProps) => {
   const t = useTranslation();
   const { providerUseCase } = useContext(GameManagementContext);
 
   const [providerNames, setProviderNames] = useState<GetProviderNamesViewModel>([]);
 
-  const selectOptions = useMemo(() => providerNames.map((c) => ({ value: c.value, label: c.label })), [providerNames]);
+  const selectOptions = useMemo(
+    () => providerNames.map((c) => ({ value: c[valueProp], label: c.label })),
+    [valueProp, providerNames]
+  );
 
   useEffect(() => {
     providerUseCase.getProviderNames(isMain).then(setProviderNames);
